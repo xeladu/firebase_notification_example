001b51d520d6dfd78c350b85baf5c8dcd3bc39f0
Alex Duschek <Alex.Duschek@hotmail.de>
2022-10-15 01:02:22 +0200

    Using new messaging API

--- functions/index.js
+++ functions/index.js
@@ -6,16 +6,21 @@ admin.initializeApp();
 const messaging = admin.messaging();
 
 exports.notifySubscribers = functions.https.onCall(async (data, _) => {
+
     try {
-        await messaging.sendToDevice(data.targetDevices, {
+        const multiCastMessage = {
             notification: {
                 title: data.messageTitle,
                 body: data.messageBody
-            }
-        });
+            },
+            tokens: data.targetDevices
+        }
+
+        await messaging.sendMulticast(multiCastMessage);
 
         return true;
+
     } catch (ex) {
-        return false;
+        return ex;
     }
 });
